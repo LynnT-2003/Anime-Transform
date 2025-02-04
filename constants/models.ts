6c3e3d18d0dec1ecdc77f272ee339604b2df7fda
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2025-02-04 17:46:48 +0700

    new models

--- constants/models.ts
+++ constants/models.ts
@@ -4,35 +4,35 @@ export const models = [
     title: "Model 1",
     image:
       "https://imgs.search.brave.com/nXZmSaMrB3oLjQ-EQED7gRUoGn2TO5sHCBjdfkdUR-A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93MC5w/ZWFrcHguY29tL3dh/bGxwYXBlci83NDcv/MzI3L0hELXdhbGxw/YXBlci10YW5qaXJv/LXBpYy1mYW4tYXJ0/LWFuaW1lLWRlbW9u/LXNsYXllci1raW1l/dHN1LW5vLXlhaWJh/LmpwZw",
-    description: "Description for Model 1",
+    description: "Hinokami Kagura",
   },
   {
     id: 2,
-    title: "Model 2",
+    title: "Ichigo - Hollow",
     image:
-      "https://imgs.search.brave.com/nXZmSaMrB3oLjQ-EQED7gRUoGn2TO5sHCBjdfkdUR-A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93MC5w/ZWFrcHguY29tL3dh/bGxwYXBlci83NDcv/MzI3L0hELXdhbGxw/YXBlci10YW5qaXJv/LXBpYy1mYW4tYXJ0/LWFuaW1lLWRlbW9u/LXNsYXllci1raW1l/dHN1LW5vLXlhaWJh/LmpwZw",
-    description: "Description for Model 2",
+      "https://prod-printler-front-as.azurewebsites.net/media/photo/183047.jpg?mode=crop&width=638&height=900&rnd=0.0.1",
+    description: "Ichigo - Hollow",
   },
   {
     id: 3,
-    title: "Model 3",
+    title: "Luffy - Gear 5th",
     image:
-      "https://imgs.search.brave.com/nXZmSaMrB3oLjQ-EQED7gRUoGn2TO5sHCBjdfkdUR-A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93MC5w/ZWFrcHguY29tL3dh/bGxwYXBlci83NDcv/MzI3L0hELXdhbGxw/YXBlci10YW5qaXJv/LXBpYy1mYW4tYXJ0/LWFuaW1lLWRlbW9u/LXNsYXllci1raW1l/dHN1LW5vLXlhaWJh/LmpwZw",
-    description: "Description for Model 3",
+      "https://4kwallpapers.com/images/wallpapers/one-piece-gear-5-luffy-2880x1800-12504.png",
+    description: "Luffy - Gear 5th",
   },
   {
     id: 4,
-    title: "Model 4",
+    title: "Gojo Satoru",
     image:
-      "https://imgs.search.brave.com/nXZmSaMrB3oLjQ-EQED7gRUoGn2TO5sHCBjdfkdUR-A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93MC5w/ZWFrcHguY29tL3dh/bGxwYXBlci83NDcv/MzI3L0hELXdhbGxw/YXBlci10YW5qaXJv/LXBpYy1mYW4tYXJ0/LWFuaW1lLWRlbW9u/LXNsYXllci1raW1l/dHN1LW5vLXlhaWJh/LmpwZw",
-    description: "Description for Model 4",
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1d6efb41-240b-4940-9f85-4507a8f6b597/di2c960-fbbce295-eadc-4ece-ad9d-a2cd104e5276.png/v1/fill/w_1280,h_2266,q_80,strp/gojo_hollow_blue_by_demonxkilla_di2c960-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjI2NiIsInBhdGgiOiJcL2ZcLzFkNmVmYjQxLTI0MGItNDk0MC05Zjg1LTQ1MDdhOGY2YjU5N1wvZGkyYzk2MC1mYmJjZTI5NS1lYWRjLTRlY2UtYWQ5ZC1hMmNkMTA0ZTUyNzYucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-JTCk_D0DVjkdGbJQ0CvAXjqRb_wgVILMdgKrtucBIk",
+    description: "Gojo Satoru",
   },
   {
     id: 5,
-    title: "Model 5",
+    title: "Ichigo - Tensa Zangetsu",
     image:
-      "https://imgs.search.brave.com/nXZmSaMrB3oLjQ-EQED7gRUoGn2TO5sHCBjdfkdUR-A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93MC5w/ZWFrcHguY29tL3dh/bGxwYXBlci83NDcv/MzI3L0hELXdhbGxw/YXBlci10YW5qaXJv/LXBpYy1mYW4tYXJ0/LWFuaW1lLWRlbW9u/LXNsYXllci1raW1l/dHN1LW5vLXlhaWJh/LmpwZw",
-    description: "Description for Model 5",
+      "https://i.pinimg.com/736x/a6/4e/bf/a64ebfc00edaa12f6666fb4221bda9ef.jpg",
+    description: "Ichigo - Tensa Zangetsu",
   },
   {
     id: 6,
